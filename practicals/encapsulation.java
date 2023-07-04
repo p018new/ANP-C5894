dadeed615e98de9b38ce6999a147c9ef2cd744ff
p018new <137863154+p018new@users.noreply.github.com>
2023-07-04 23:12:08 +0530

    Add files via upload

---
+++ practicals/encapsulation.java
@@ -0,0 +1,62 @@
+package oops;
+
+class encapsulation 
+{
+	private String name;
+	private int age;
+	//getName method
+
+	public String getName() 
+	{
+		return name;
+	}
+	//setName method
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	//getAge method
+
+	public int getAge()
+	{
+		return age;
+	}
+	//getAge method
+
+	public void setAge(int age) 
+	{
+		this.age = age;
+	}
+
+	  public void setRoll(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getRoll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	}
+
+class main
+{
+
+	public static void main(String[] args)
+	{
+		//Creating instance of class Encapsulation
+				encapsulation e=new encapsulation();
+				
+				//Setting values
+				e.setName("Pooja");
+				e.setAge(23);
+				
+				//Print values
+				System.out.println("Name is:" + e.getName());
+				System.out.println("Age is:" + e.getAge());
+	}
+}
---
+++ practicals/helloRain.java
@@ -0,0 +1,8 @@
+//to print simple "Hello Rain" 
+class HelloRain
+{
+public static void main(String[] args)
+{
+System.out.println("Hello Rains");
+}
+}
