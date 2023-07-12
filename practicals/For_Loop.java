459e803d8a250d91e3d3b7ebf5eb00065d447ff1
p018new <137863154+p018new@users.noreply.github.com>
2023-07-12 12:59:05 +0530

    Add files via upload

---
+++ practicals/For_Loop.java
@@ -0,0 +1,11 @@
+package loops;
+class For_Loop
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println("Values of i is :" +i);
+        }
+    }
+}
